b1773191c2c519651481f1830371297c9efd98d9
uno <st9007a@gmail.com>
2017-08-31 14:33:24 +0800

    material

---
+++ Assets/Scripts/PowerCubeBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerCubeBehaviour : MonoBehaviour {
+
+	public float radius = 1;
+	public float speed;
+	public float offset;
+
+	private float rotateAngle;
+
+	void Awake () {
+		rotateAngle = offset;
+	}
+	
+	void Update () {
+		transform.localPosition = new Vector3(transform.localPosition.x, Mathf.Cos(rotateAngle) * radius, Mathf.Sin(rotateAngle) * radius);
+		rotateAngle += speed * Time.deltaTime;
+	}
+}
